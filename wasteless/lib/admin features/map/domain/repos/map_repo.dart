d3d2a82c4054c31a4e50b49eb869be9ff9e98ca1
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2023-03-11 18:56:19 +0300

    map added

--- wasteless/lib/admin features/map/domain/repos/map_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:wasteless/core/errors/failure.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class MapRepo {
-  Future<Either<Failure, List>> getAllMapItems();
-  Future<Either<Failure, List>> filterMapItems(List filterdItems);
-  displayDetails(String objectType, Object opject);
-}
