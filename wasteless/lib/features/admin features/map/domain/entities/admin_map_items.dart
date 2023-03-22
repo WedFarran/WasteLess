6bff88b1f40e39d1b9ebe878d35b5b015170e415
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2023-03-22 04:27:09 +0300

    working on the domain layer agin

---
+++ wasteless/lib/features/admin features/map/domain/entities/admin_map_items.dart
@@ -0,0 +1,32 @@
+import 'package:wasteless/features/admin%20features/map/domain/entities/bin_entity.dart';
+import 'package:wasteless/features/admin%20features/map/domain/entities/driver_entity.dart';
+
+/*class AdminMapItems implements AdminMapDriver implements AdminMapBin{
+  @override
+  // TODO: implement area
+  String get area => throw UnimplementedError();
+
+  @override
+  // TODO: implement fullName
+  String get fullName => throw UnimplementedError();
+
+  @override
+  // TODO: implement image
+  String get image => throw UnimplementedError();
+
+  @override
+  // TODO: implement lat
+  double get lat => throw UnimplementedError();
+
+  @override
+  // TODO: implement lng
+  double get lng => throw UnimplementedError();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+
+  @override
+  // TODO: implement stringify
+  bool? get stringify => throw UnimplementedError();
+}*/
