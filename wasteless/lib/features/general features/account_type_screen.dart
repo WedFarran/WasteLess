9c74a4977e4f1041329c528583b2e1f777e6d97b
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2023-04-06 16:25:13 +0300

    choose account screen ui semi done

--- wasteless/lib/features/general features/account_type_screen.dart
+++ wasteless/lib/features/general features/account_type_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:wasteless/core/utils/colors.dart';
+import 'package:wasteless/features/general%20features/widgets/choose_account_decoration_widget.dart';
 import '../../core/utils/assets_path.dart';
 import '../admin features/login/presentation/screens/admin_login.dart';
 import '../driver features/login/presentation/screens/driver_login.dart';
@@ -11,22 +12,41 @@ class AccountType extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
+      body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            const ChooseAccountDecorationWidget(
+              primaryColor: PRIMARY_BLUE,
+              right: -30,
+              bottom: 25,
+              rotation: 40,
+              borderRadius: 50,
+              height: 0.15,
+              width: 0.50,
+            ),
             Image.asset(WASTELESS_LOGO),
             InkWell(
-              child: Text('Aaministrator'),
+              child: const Text('Aaministrator'),
               onTap: () {
                 Navigator.pushNamed(context, AdminLogIn.id);
               },
             ),
             InkWell(
-              child: Text('Driver'),
+              child: const Text('Driver'),
               onTap: () {
                 Navigator.pushNamed(context, DriverLogIn.id);
               },
-            )
+            ),
+            const ChooseAccountDecorationWidget(
+              primaryColor: PRIMARY_GREEN,
+              left: -90,
+              bottom: -20,
+              rotation: -50,
+              borderRadius: 95,
+              height: 0.35,
+              width: 0.60,
+            ),
           ],
         ),
       ),
