e07e3ae2689d99c4d29da6289ce516a3df50f890
Noura Al Harbi <noura6alharbii@gmail.com>
2023-04-07 04:54:44 +0300

    account type

--- wasteless/lib/features/general features/account_type_screen.dart
+++ wasteless/lib/features/general features/account_type_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wasteless/core/utils/colors.dart';
+import 'package:wasteless/core/utils/media_query.dart';
 import 'package:wasteless/features/general%20features/widgets/choose_account_decoration_widget.dart';
 import '../../core/utils/assets_path.dart';
 import '../admin features/login/presentation/screens/admin_login.dart';
@@ -25,18 +26,81 @@ class AccountType extends StatelessWidget {
               height: 0.15,
               width: 0.50,
             ),
-            Image.asset(WASTELESS_LOGO),
+            Image.asset(
+              WASTELESS_LOGO,
+              height: context.height * 0.25,
+            ),
             InkWell(
-              child: const Text('Aaministrator'),
+              child: Container(
+                child: Text(
+                  'Aaministrator',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w700,
+                      color: PRIMARY_BLUE,
+                      shadows: [
+                        Shadow(
+                            color: BLACK.withOpacity(0.25),
+                            blurRadius: 4.0,
+                            offset: Offset(0, 4))
+                      ]),
+                ),
+                height: context.height * 0.15,
+                width: context.width * 0.6,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: WHITE,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 1),
+                          blurRadius: 4,
+                          color: BLACK.withOpacity(0.25))
+                    ]),
+              ),
               onTap: () {
                 Navigator.pushNamed(context, AdminLogIn.id);
               },
             ),
+            SizedBox(
+              height: context.height * 0.05,
+            ),
             InkWell(
-              child: const Text('Driver'),
+              child: Container(
+                child: Text(
+                  'Driver',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w700,
+                      color: PRIMARY_GREEN,
+                      shadows: [
+                        Shadow(
+                            color: BLACK.withOpacity(0.25),
+                            blurRadius: 4.0,
+                            offset: Offset(0, 4))
+                      ]),
+                ),
+                height: context.height * 0.15,
+                width: context.width * 0.6,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: WHITE,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 1),
+                          blurRadius: 4,
+                          color: BLACK.withOpacity(0.25))
+                    ]),
+              ),
               onTap: () {
                 Navigator.pushNamed(context, DriverLogIn.id);
               },
+            ),
+            SizedBox(
+              height: context.height * 0.05,
             ),
             const ChooseAccountDecorationWidget(
               primaryColor: PRIMARY_GREEN,
