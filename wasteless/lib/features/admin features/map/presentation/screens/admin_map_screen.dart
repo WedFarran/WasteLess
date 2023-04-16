a7cc49bf741d94c31094849b19bbcec7c8684778
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2023-04-16 13:34:01 +0300

    he

--- wasteless/lib/features/admin features/map/presentation/screens/admin_map_screen.dart
+++ wasteless/lib/features/admin features/map/presentation/screens/admin_map_screen.dart
@@ -3,10 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../../../../../core/widgets/map_widgets/filtering_button.dart';
+import 'package:wasteless/core/utils/media_query.dart';
+import '../../../../../core/utils/assets_path.dart';
+import '../../../../../core/utils/colors.dart';
+import '../../../../../core/utils/styles.dart';
 import '../../data/models/bins_models.dart';
 import '../../data/models/driver_models.dart';
 import '../map_tools.dart';
+import '../widgets/filtering_button_widget.dart';
 
 class AdminMapScreen extends StatefulWidget {
   static const String id = 'admin_map_screen';
@@ -22,10 +26,10 @@ class _AdminMapScreenState extends State<AdminMapScreen> {
   late DatabaseReference dbDrivers;
   List<BinsModel> binsList = [];
   List<DriversModel> driversList = [];
-  bool fullCheck = false;
-  bool halfFullCheck = false;
-  bool emptyCheck = false;
-  bool driversCheck = false;
+  bool fullCheck = true;
+  bool halfFullCheck = true;
+  bool emptyCheck = true;
+  bool driversCheck = true;
 
   final LatLng _initialPosition =
       const LatLng(21.42462845849512, 39.82612550889805);
@@ -86,39 +90,94 @@ class _AdminMapScreenState extends State<AdminMapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FilteringButton(
-          driversOnTap: () {
-            setState(() {
-              driversCheck = !driversCheck;
-            });
-          },
-          driversSelected: driversCheck,
-          fullOnTap: () {
-            setState(() {
-              fullCheck = !fullCheck;
-            });
-          },
-          fullSelected: fullCheck,
-          halfFullOnTap: () {
-            setState(() {
-              halfFullCheck = !halfFullCheck;
-            });
-          },
-          halfFullSelected: halfFullCheck,
-          emptyOnTap: () {
-            setState(() {
-              emptyCheck = !emptyCheck;
-            });
-          },
-          emptySelected: emptyCheck,
-          resetOnTap: () {
-            setState(() {
-              emptyCheck = true;
-              halfFullCheck = true;
-              fullCheck = true;
-              driversCheck = true;
-            });
-          },
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 80.0),
+          child: FloatingActionButton(
+            backgroundColor: WHITE,
+            child: Image.asset(
+              FILTRING_ICON,
+              height: context.height * 0.04,
+            ),
+            onPressed: () => showModalBottomSheet(
+                shape: const RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(40))),
+                backgroundColor: WHITE,
+                context: context,
+                builder: (context) => Container(
+                      height: context.height * 0.33,
+                      margin: const EdgeInsets.only(
+                          top: 15, right: 20, bottom: 5, left: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Image.asset(
+                                    FILTRING_ICON,
+                                    height: context.height * 0.02,
+                                  ),
+                                  Text(
+                                    'Filter',
+                                    style: anyColorSize16(FONT_GRAY),
+                                  )
+                                ],
+                              ),
+                              InkWell(
+                                onTap: () => setState(() {
+                                  driversCheck = true;
+                                  emptyCheck = true;
+                                  fullCheck = true;
+                                  halfFullCheck = true;
+                                }),
+                                child:
+                                    Text('Reset', style: anyColorSize16(BLACK)),
+                              ),
+                            ],
+                          ),
+                          FilteringButtonWidget(
+                            onTap: () => setState(() {
+                              fullCheck = !fullCheck;
+                            }),
+                            selected: fullCheck,
+                            icon: Full_BIN_ICON,
+                            status: 'Full',
+                          ),
+                          FilteringButtonWidget(
+                            onTap: () => setState(() {
+                              halfFullCheck = !halfFullCheck;
+                            }),
+                            selected: halfFullCheck,
+                            icon: HALF_FULL_BIN_ICON,
+                            status: 'Half Full',
+                          ),
+                          FilteringButtonWidget(
+                            onTap: () => setState(() {
+                              emptyCheck = !emptyCheck;
+                            }),
+                            selected: emptyCheck,
+                            icon: EMPTY_BIN_ICON,
+                            status: 'Empty',
+                          ),
+                          const Divider(
+                            color: PRIMARY_BLUE,
+                            thickness: 1,
+                          ),
+                          FilteringButtonWidget(
+                            onTap: () => setState(() {
+                              driversCheck = !driversCheck;
+                            }),
+                            selected: driversCheck,
+                            icon: VEHICLE_ICON,
+                            status: 'Drivers',
+                          ),
+                        ],
+                      ),
+                    )),
+          ),
         ),
         body: SafeArea(
             child: GoogleMap(
